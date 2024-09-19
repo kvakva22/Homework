c9e3177986cbf78e2ade3fae29f32b9aae17a768
kvakva22 <qwexykk@gmail.com>
2024-09-19 19:46:14 +0300

    Update homework_1.py

--- homework_1.py
+++ homework_1.py
@@ -11,4 +11,5 @@ def get_days_from_today(date):
         date1 = datetime.strptime(date, "%Y.%m.%d").date()
         result = date1 - datetime.today().date()
         return result.days
+
 print(get_days_from_today(date_input))
