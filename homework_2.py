b12c7157d01205d64b72eedd62b630067a6e16d0
kvakva22 <qwexykk@gmail.com>
2024-09-19 19:45:54 +0300

    Update homework_2.py

--- homework_2.py
+++ homework_2.py
@@ -1,13 +1,23 @@
 import random
+
+
 def get_numbers_ticket(min, max, quantity):
     lotterylist = []
-    if min < 1:
-        print(lotterylist)
-    if max > 1000:
-        print(lotterylist)
-    for number in range(quantity):
-        number=random.sample(range(min, max), k=1)
+
+    if min < 1 or max > 1000:
+        print("Числа повинні знаходитись у діапазоні від 1 до 1000")
+        return lotterylist
+    
+    if min > max:
+        print ("Мінімальне значення більше максимального. Будь ласка, перевірте введені дані!")
+        return lotterylist
+    
+    try:
+        number=random.sample(range(min, max), k=quantity)
         lotterylist=number
+    except  ValueError:
+        print(f"Неможливо обрати {quantity} значень з діапазону від {min} до {max}")
     return lotterylist
 
-print(get_numbers_ticket(4,19,4))
+
+print(get_numbers_ticket(1,49,6))
